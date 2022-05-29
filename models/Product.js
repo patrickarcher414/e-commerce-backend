25d392b5297d6f9c9ce13f058758bb29d518abf0
patrickarcher414 <patrickarcher414@gmail.com>
2022-05-29 14:52:38 -0500

    Product model and npm install

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,27 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    prduct_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      isDecimal: true,
+    },
+    stock: {
+      type: DataTypes.INT,
+      allowNull: false,
+      defaultValue: 10,
+      isNumeric: true,
+    },
   },
   {
     sequelize,
